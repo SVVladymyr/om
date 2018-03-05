9e9b5bc3a751915fc0835c7cb0b1e606b41424de
Ivan [Front-Department] <vaneasmoyks@gmail.com>
2018-03-05 11:42:50 +0200

    cost items

--- resources/views/cost_items/cost_item.blade.php
+++ resources/views/cost_items/cost_item.blade.php
@@ -1,7 +1,15 @@
-
+<div ng-controller="specification" class="main-body ng-scope flex" data-ui-view="" data-flex="">
+    <md-card class="md-table ng-scope _md">
+    <md-card-content>
+      <div class="md-table-loader" data-ng-if="!loaded">
+            <md-progress-circular md-mode="indeterminate"></md-progress-circular>
+        </div>
 
           <tr>
 {{--              <td>{{ $cost_item->id }}</td>--}}
               <td>{{ $cost_item->name }}
                <a data-id="{{ $cost_item->id }}" class="btn btn-large btn-primary edit-togle open-modal-cost-item-edit" style="margin: 0; float: right;" href="#"></a></td>
           </tr>
+</md-card-content>
+</md-card>
+</div>
