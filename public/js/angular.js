ca60819cd71a0426089b25bad0defb5b7634265c
Yan <gorohovik77@gmail.com>
2018-03-12 12:12:30 +0200

    asd

--- public/js/angular.js
+++ public/js/angular.js
@@ -99,9 +99,36 @@ mainApp.controller('orders', [
 mainApp.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
   $scope.myDate;
   $scope.changedate = function(){
-      var now = $scope.myDate;
-      var formated_date = now.format("yyyy-mm-dd");
-      $scope.created_from =  formated_date;
+      let createdFrom = $scope.createdFrom;
+      if(!!createdFrom){
+        let createdFromformated_date = createdFrom.format("yyyy-mm-dd");
+        $scope.created_from =  createdFromformated_date;
+      }
+      let expectedDeliveryFrom = $scope.expectedDeliveryFrom;
+      if(!!expectedDeliveryFrom){
+        let expectedDeliveryFromformated_date = expectedDeliveryFrom.format("yyyy-mm-dd");
+        $scope.expected_delivery_from =  expectedDeliveryFromformated_date;
+      }
+      let createdToDel = $scope.createdToDel;
+      if(!!createdToDel){
+        let createdToDelformated_date = createdToDel.format("yyyy-mm-dd");
+        $scope.created_toDel =  createdToDelformated_date;
+      }
+      let expectedDeliveryToDel = $scope.expectedDeliveryToDel;
+      if(!!expectedDeliveryToDel){
+        let expectedDeliveryToDelformated_date = expectedDeliveryToDel.format("yyyy-mm-dd");
+        $scope.expected_delivery_toDel =  expectedDeliveryToDelformated_date;
+      }
+      let createdToOrd = $scope.createdToOrd;
+      if(!!createdToOrd){
+        let createdToOrdformated_date = createdToOrd.format("yyyy-mm-dd");
+        $scope.created_toOrd =  createdToOrdformated_date;
+      }
+      let expectedDeliveryToOrd = $scope.expectedDeliveryToOrd;
+      if(!!expectedDeliveryToOrd){
+        let expectedDeliveryToOrdformated_date = expectedDeliveryToOrd.format("yyyy-mm-dd");
+        $scope.expected_delivery_toOrd =  expectedDeliveryToOrdformated_date;
+      }
   }
   // duble
   $scope.ChangeChechbox = function (){
@@ -111,6 +138,27 @@ mainApp.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
     for(let item in $scope.selectedVegetables){
         document.getElementById($scope.selectedVegetables[item]).checked=true;
       }
+    for(let item in $scope.defaultClientOrder){
+        document.getElementById($scope.defaultClientOrder[item]).checked=false;
+      }
+    for(let item in $scope.selectedVegetablesClient){
+        document.getElementById($scope.selectedVegetablesClient[item]).checked=true;
+      }
+
+  }
+  $scope.defaultClientOrderOnload = function(){
+    $scope.defaultStatusOrderOnload = {};
+    $scope.defaultClientOrderOnload = {};
+    for(let item in $scope.defaultStatusOrder){
+        if(document.getElementById($scope.defaultStatusOrder[item]).checked){
+        $scope.defaultStatusOrderOnload[$scope.defaultStatusOrder[item]] = true;
+        }
+    }
+    for(let item in $scope.defaultClientOrder){
+        if(document.getElementById($scope.defaultClientOrder[item]).checked){
+          $scope.defaultClientOrderOnload[$scope.defaultClientOrder[item]] = true;
+        }
+    }
   }
   // end duble
     $scope.close = function () {
